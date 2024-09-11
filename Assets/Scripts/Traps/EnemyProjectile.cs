91266ae90d6979d736d37c7476110d75ed8f20f8
Nikola Lazarevski <nikola@sorsixs-MBP-3.home>
2024-09-11 20:47:33 +0200

    add n_level

--- Assets/Scripts/Traps/EnemyProjectile.cs
+++ Assets/Scripts/Traps/EnemyProjectile.cs
@@ -4,6 +4,7 @@ public class EnemyProjectile : EnemyDamage
 {
     [SerializeField] private float speed;
     [SerializeField] private float resetTime;
+    [SerializeField] private float direction;
     private float lifetime;
     private Animator anim;
     private BoxCollider2D coll;
@@ -20,6 +21,7 @@ public class EnemyProjectile : EnemyDamage
     {
         hit = false;
         lifetime = 0;
+        if (direction == 0) direction = 1;
         gameObject.SetActive(true);
         coll.enabled = true;
     }
@@ -27,7 +29,7 @@ public class EnemyProjectile : EnemyDamage
     {
         if (hit) return;
         float movementSpeed = speed * Time.deltaTime;
-        transform.Translate(movementSpeed, 0, 0);
+        transform.Translate(movementSpeed * direction, 0, 0);
 
         lifetime += Time.deltaTime;
         if (lifetime > resetTime)
