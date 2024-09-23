ed592925cb3da842c9f5acf3fe59340dabf42a54
Kr-Stam <kristijanstameski@gmail.com>
2024-09-23 20:54:58 +0200

    Revert "Merge branch 'main' of https://github.com/n-lazarevski/TeamProject into kiko"
    This reverts commit cfea909840af1d849c1c611a997c5b17d8a3fef4, reversing
    changes made to bc28f81ba53c0cdec271f02ad9bfd5e76c52843b.

--- Assets/Scripts/Traps/EnemyDamage.cs
+++ Assets/Scripts/Traps/EnemyDamage.cs
@@ -9,13 +9,4 @@ public class EnemyDamage : MonoBehaviour
         if (collision.tag == "Player")
             collision.GetComponent<Health>()?.TakeDamage(damage);
     }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            collision.rigidbody.GetComponent<Health>().TakeDamage(damage);
-        }
-    }
-
 }
